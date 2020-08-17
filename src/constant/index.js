c6d3816d11439b3aa903612cf4fa436b2143d5bd
lucifer <xifa@tsign.cn>
2020-08-17 10:39:16 +0800

    feat: 更改仓库地址

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,5 +1,6 @@
 export const LEETCODE_CN_URL = "https://leetcode-cn.com";
 export const LEETCODE_URL = "https://leetcode.com";
-export const ISSUES_URL = "https://github.com/azl397985856/leetcode/issues";
+export const ISSUES_URL =
+  "https://github.com/azl397985856/leetcode-cheat/issues";
 export const CONTRIBUTE_COMPANY_URL =
-  "https://github.com/azl397985856/leetcode/issues/382";
+  "https://github.com/azl397985856/leetcode-cheat/issues/1";
