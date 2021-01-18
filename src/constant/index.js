cd974a339468d8362a4ff03710d4bcc866e48571
azl397985856 <azl397985856@users.noreply.github.com>
2021-01-18 07:55:13 +0000

    deploy: 5023fd7a7a5302cb326f8509c915aeebb64129b5

--- src/constant/index.js
+++
@@ -1,9 +0,0 @@
-export const LEETCODE_CN_URL = "https://leetcode-cn.com";
-export const LEETCODE_URL = "https://leetcode.com";
-export const ISSUES_URL =
-  "https://github.com/azl397985856/leetcode-cheat/issues";
-export const CONTRIBUTE_COMPANY_URL =
-  "https://github.com/azl397985856/leetcode-cheat/issues/1";
-
-export const CONTRIBUTE_PROGRAMMING_LANGUAGE_URL =
-  "https://github.com/azl397985856/leetcode-cheat/issues/4";
