826709dce6e001d4cb91dc2bd5540bb85f54f49f
robot <xifa@tsign.cn>
2021-10-01 14:13:34 +0800

    feat: v0.9.2

--- src/App.js
+++ src/App.js
@@ -34,9 +34,17 @@ const { TabPane } = Tabs;
 
 const dataSource = Object.values(problems);
 const chrome = window.chrome;
+
 // function inLeetCodeWebsite(url) {
 //   return [LEETCODE_CN_URL, LEETCODE_URL].some((u) => url.includes(u));
 // }
+
+// 根据 Github 用户名获取用户信息
+function getUserInfo(username) {
+  return fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+    .then((res) => res);
+}
 
 const columns = [
   {
