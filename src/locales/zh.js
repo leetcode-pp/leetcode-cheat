35c3dfe0081c228d80f84d38205dc39184d2f922
Renkin <906155099@qq.com>
2023-10-31 14:13:25 +0800

    feat: support insert button on en website

--- src/locales/zh.js
+++ src/locales/zh.js
@@ -2,7 +2,7 @@ export const cn = {
   app: {
     back: "返回主页",
     viewSolution: "查看本题题解",
-    viewInHandpickCollection:"该题已被收录到精选合集《{slotText}》点击查看",
+    viewInHandpickCollection: "该题已被收录到精选合集《{slotText}》点击查看",
     notCollected: "本题暂未被力扣加加收录，点击查看所有已收录题目~",
     allCollected: "所有已收录的题目",
     writingExplanation: "正在撰写题解...",
@@ -18,6 +18,20 @@ export const cn = {
         tag: "标签",
       },
     },
+    initializeContentScriptFailed:"初始化 chrome 插件 content script 失败",
+    run: "运行",
+    submit: "提交",
+    wirteSolution: "写题解",
+    visualizationDebug: "可视化调试",
+    linkedListCommnet:
+      "如果你在调试链表题目，手动生成链表很麻烦，想快速生成链表可以注释如下方法，并使用如下方法，输入一个数组，返回一个链表",
+    visualDebugComment:
+      "替换下方的 xxx 为主函数名， yyy 为测试用例参数开启调试",
+    visualDebugSupport: "当前仅支持",
+    getProblemError: "获取题目描述失败，请先切换到题目描述标签页",
+    savingProblem: "正在存储题目信息，请稍后~",
+    githubAPIError:
+      "使用 Github API 失败，已为您切换为普通模式，普通模式仅可自动带入题目名称，题目地址以及题解语言。",
   },
   codeTemplate: {
     name: "代码模板",
