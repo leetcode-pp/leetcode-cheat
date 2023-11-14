6dce5eab5da12be2abdef4ac26127ec011b309c1
azl397985856 <azl397985856@users.noreply.github.com>
2023-11-14 06:08:40 +0000

    deploy: ab4c42d1d1a18288ad902df7a84733e6bb146b66

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/leetcode-cheat/precache-manifest.c7396b8016bc6e5d5af3eec3551cc63d.js"
+  "/leetcode-cheat/precache-manifest.66556ab8d254de2365bf6866ee7f1cec.js"
 );
 
 self.addEventListener('message', (event) => {
